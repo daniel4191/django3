d96ef9783a0e98c00444706142bb0389e2259768
daniel4191 <daniell8282@naver.com>
2025-10-04 15:21:53 +0900

    회원가입 기능

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
-from .forms import LoginForm
+from .forms import LoginForm, SignupForm
+from .models import User
 
 # Create your views here.
 def login_view(request):
@@ -10,15 +12,43 @@ def login_view(request):
         # LoginForm 인스턴스를 만들며, 입력 데이터는 request.POST를 사용
         form = LoginForm(data = request.POST)
         
-        # LoginForm에 들어온 데이터가 적절한지 유효성 검사
-        print("form.is_valid():", form.is_valid())
-        
-        # 유효성 검사 이후에는 cleaned_data에서 데이터를 가져와 사용
-        print("form.cleaned_data:", form.cleaned_data)
+        if form.is_valid():
+            
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            
+            #username, password 등록 데이터랑 매치하는지
+            user = authenticate(username = username, password = password)
+            
+            # 등록 데이터와 매치가 된다면
+            if user:
+                login(request, user)
+                return redirect("/posts/feeds/")
+            else:
+                form.add_error(None, "입력한 자격증명에 해당하는 사용자가 없습니다.")
+                
+        # 어떤 경우든 실패한 경우 - 데이터검증, 사용자 검사 다시 LoginForm을 사용한 로그인 페이지 렌더링
         context = {"form":form}
-    
+        return render(request, "users/login.html", context)
     else:
         form = LoginForm()
-        context = {"form": form}
+        context = {"form":form}
+        return render(request, "users/login.html", context)
+    
+def logout_view(request):
+    logout(request)
+    return redirect("/users/login/")
+
+def signup(request):
+    if request.method == "POST":
+        form = SignupForm(data = request.POST, files = request.FILES)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("/posts/feeds/")
         
-    return render(request, "users/login.html", context)
+    else:
+        form = SignupForm()
+        
+    context = {"form": form}
+    return render(request, "users/signup.html", context)
